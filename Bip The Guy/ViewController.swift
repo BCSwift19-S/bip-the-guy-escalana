aa35291a0206ace439621b344d1f855af6c631bb
Alessandra Escalante <escalana@bc.edu>
2019-02-09 13:57:33 -0500

    4.3 completed and fixed the animation on the picture

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     //Properties
 
-    @IBOutlet weak var ImagetoPunch: UILabel!
+
+    @IBOutlet weak var ImageToPunch: UIImageView!
+    
+    var audioplayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +24,29 @@ class ViewController: UIViewController {
     }
     //Functions
     func animateImage(){
-        let bounds = self.ImagetoPunch.bounds
+        let bounds = self.ImageToPunch.bounds
         let shrinkValue: CGFloat = 60
         //shrink our imageToPunch by 60 pixels -- i dont rlly understadn the diff between x,y and width,height-- ask
-        self.ImagetoPunch.bounds = CGRect(x: self.ImagetoPunch.bounds.origin.x + shrinkValue, y: self.ImagetoPunch.bounds.origin.y + shrinkValue , width: self.ImagetoPunch.bounds.size.width - shrinkValue, height: self.ImagetoPunch.bounds.size.height - shrinkValue)
+        self.ImageToPunch.bounds = CGRect(x: self.ImageToPunch.bounds.origin.x + shrinkValue, y: self.ImageToPunch.bounds.origin.y + shrinkValue , width: self.ImageToPunch.bounds.size.width - shrinkValue, height: self.ImageToPunch.bounds.size.height - shrinkValue)
         
-        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations:{self.ImagetoPunch.bounds = bounds}, completion: nil)
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations:{self.ImageToPunch.bounds = bounds}, completion: nil)
+    }
+    // Sounds Function
+        func playSound(soundname:String, audioPlayer: inout AVAudioPlayer){
+            //can we load in the file soundName?
+            if let sound = NSDataAsset(name: soundname){
+                //check if sound.data is a sound file
+                do{
+                    try audioPlayer = AVAudioPlayer(data: sound.data)
+                    audioPlayer.play()
+                }catch{
+                    //if sound.data is not a valid audio file
+                    print("ERROR: data in \(soundname) couldn't be played as a sound.")
+                }
+            }else{
+                // if reading in the NSDataAsset didn't work, tell user/ report the error
+                print("ERROR: file \(soundname) didn't load")
+        }
     }
     // Actions
     @IBAction func CameraPressed(_ sender: UIButton) {
@@ -36,6 +57,8 @@ class ViewController: UIViewController {
     
     @IBAction func ImageTapped(_ sender: UITapGestureRecognizer) {
         print("Image tapped!")
+        animateImage()
+        playSound(soundname: "PunchSound", audioPlayer: &audioplayer)
     }
     
     
